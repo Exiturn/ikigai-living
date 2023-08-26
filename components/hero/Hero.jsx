3864f98ee548b49977882d3e1579222b3d49b138
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-08-26 21:17:50 +0100

    Removed Images from Hero Component

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -15,32 +15,6 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-row flex-wrap">
-          {}
-
-          {/* <Image
-          src={Images[0]}
-          width={400}
-          height={400}
-          alt="loader image 1"
-        />
-        <Image 
-          src={Images[1]}
-          width={400}
-          height={400}
-          alt="loader image 2"
-        />
-        <Image 
-          src={Images[2]}
-          width={400}
-          height={400}
-          alt="loader image 3"
-        />
-        <Image 
-          src={Images[3]}
-          width={400}
-          height={400}
-          alt="loader image 4"
-        /> */}
         </div>
       </div>
     </section>
